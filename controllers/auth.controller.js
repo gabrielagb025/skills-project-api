62d46e6f3bcc5d1fccd2d14092ffa3beb56d65f9
Gabriela García <ggarciaborregon@gmail.com>
2023-10-03 17:20:45 +0200

    changed req body to user data

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -4,11 +4,13 @@ const createError = require('http-errors');
 const jwt = require('jsonwebtoken');
 
 module.exports.register = (req, res, next) => {
-    if (req.file) {
-        req.body.avatar = req.file.path;
+    
+    const userData = {
+        ...req.body,
+        avatar: req.file ? req.file.path : undefined
     }
 
-    User.create(req.body)
+    User.create(userData)
     .then(user => res.status(StatusCodes.CREATED).json(user))
     .catch(next)
 }
