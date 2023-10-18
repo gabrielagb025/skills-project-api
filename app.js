7002aadfb34ff0787dcde470c5743e390bc15997
Gabriela García <ggarciaborregon@gmail.com>
2023-10-18 19:26:40 +0200

    cors

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ require('./config/db.config');
 
 const app = express();
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173']
+    origin: process.env.CORS_ORIGIN || ['http://localhost:5173', 'http://127.0.0.1:5173']
 }))
 
 app.use(logger('dev'));
--- models/message.model.js
+++ models/message.model.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const messageSchema = new mongoose.Schema({
     text: {
-        type: String
+        type: String,
+        required: true
     },
     sender: {
         type: mongoose.Schema.Types.ObjectId,
